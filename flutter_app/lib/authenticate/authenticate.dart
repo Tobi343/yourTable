d31510db142fb9c8ba93823eb4068d8d24a4d953
SebastianStecher <47062722+SebastianStecher@users.noreply.github.com>
2021-10-19 15:14:16 +0200

    Update authenticate.dart

--- flutter_app/lib/authenticate/authenticate.dart
+++ flutter_app/lib/authenticate/authenticate.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 
 class AuthService{
 
-  static const SERVER_IP = 'https://yourtable.herokuapp.com';
+  static const SERVER_IP = 'http://34.139.54.192';
   final storage = FlutterSecureStorage();
 
 
